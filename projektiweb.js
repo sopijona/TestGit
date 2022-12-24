3c7d7c786b9a8ba667ea679553177ba2c27207e8
sopijona <117680198+sopijona@users.noreply.github.com>
2022-12-24 19:40:00 +0100

    Comit From Erijona Sopi

---
+++ projektiweb.js
@@ -0,0 +1,30 @@
+var pht1 = document.querySelector("img");
+function showPht1(){
+    pht1.style.marginLeft="0";
+}
+var pht2 = document.querySelector("img");
+function showPht2(){
+    pht2.style.marginLeft="-25%";
+}
+var pht3 = document.querySelector("img");
+function showPht3(){
+    pht3.style.marginLeft="-50%";
+}
+var pht4 = document.querySelector("img");
+function showPht4(){
+    pht4.style.marginLeft="-75%";
+}
+var pht5 = document.querySelector("img");
+function showPht5(){
+    pht5.style.marginLeft="-100%";
+}
+var pht6 = document.querySelector("img");
+function showPht6(){
+    pht6.style.marginLeft="-125%";
+}
+/*
+var username =/^w+[._-]?\w+@[a-z]\.[a-z]{2,,3}/;
+var password =/^[A-Z][a-z]{8,20}/
+
+var login = document.getElementsByClassName("button");
+*/
